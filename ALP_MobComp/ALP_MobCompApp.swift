b59936eeae96f33df277081a108bce5a9b327392
Bernardo Frederick Kowe <bfrederick@student.ciputra.ac.id>
2025-11-25 23:55:24 +0700

    Initial Commit

---
+++ ALP_MobComp/ALP_MobCompApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MobCompApp.swift
+//  ALP_MobComp
+//
+//  Created by Stefanie Agahari on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MobCompApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
